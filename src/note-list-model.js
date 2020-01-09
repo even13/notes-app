c90977b0042d9278ae31acd3e01b977b49e3add6
Eve Noirault <student@Makerss-MacBook-Air.local>
2020-01-09 16:22:37 +0000

    add note id

--- src/note-list-model.js
+++ src/note-list-model.js
@@ -1,6 +1,7 @@
 (function(exports) {
   function List() {
     this.notes = [];
+    this.id = 0;
   }
 
   List.prototype.store = function(note) {
@@ -9,7 +10,9 @@
 
   List.prototype.create = function(input) {
     note = new Note(input);
-    this.notes.push(note);
+    note.id = this.id;
+    this.id ++;
+    this.store(note);
     console.log(note);
   }
 
--- tests/note-list-test.js
+++ tests/note-list-test.js
@@ -28,3 +28,16 @@ function testListCreateNote() {
 
 console.log('testListCreateNote');
 console.log(testListCreateNote());
+
+function testNoteHasID() {
+  var list = new List();
+  list.create("new note");
+  assert.isTrue(list.notes[0].id === 0)
+  console.log(list.notes[0].id)
+  list.create("second note")
+  assert.isTrue(list.notes[1].id === 1)
+  console.log(list.notes[1].id)
+};
+
+console.log('testNoteHasID');
+console.log(testNoteHasID());
